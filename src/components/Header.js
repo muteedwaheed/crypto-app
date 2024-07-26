2971ebfd97310dd3745815bbac4eb928528d2bf6
muteeb waheed <muteebwaheed6@gmail.com>
2024-07-25 23:27:45 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import {HStack,Button} from "@chakra-ui/react"
+const Header = () => {
+  return (
+  <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"} >
+    <Button variant={"unstyled"} color={"white"}>
+      <Link to="/">Home</Link>
+    </Button>
+    <Button variant={"unstyled"} color={"white"}>
+      <Link to="/Exchanges">Exchanges</Link>
+    </Button>
+    <Button variant={"unstyled"} color={"white"}>
+      <Link to="/Coins">Coins</Link>
+    </Button>
+  </HStack>
+  ) 
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import btcSrc from "../assets/img.png";
+const Home = () => {
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <Image w={"full"} h={"full"} objectFit={"contain"} src={btcSrc} filter={"grayscale(1)"} />
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={-20}
+      >
+        hello
+      </Text>
+    </Box>
+  );
+};
+
+export default Home;
